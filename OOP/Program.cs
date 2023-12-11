061a0bf776e1f6c829d76ae2fea3ea06bee21373
nienull <62888372+nienull@users.noreply.github.com>
2023-12-11 16:23:25 +0100

    Update Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -3,3 +3,6 @@
 Class1 c = new Class1();
 string persnInfo = c.Person("John", 12345678, "mm@gmail.com");
 Console.WriteLine(persnInfo);
+
+// I skal ikke lave en tilfældigt klasse og implementer en Person metode, men lave
+// en "person klasse" med "members" som beskriver hvad personen har af egenskaber.
